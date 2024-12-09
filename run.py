eca4fee0e5488d0f4e86c1060113066e781b33fe
Vinsengi Ns <nvip2020@gmail.com>
2024-12-09 11:23:35 +0000

    Add Game Procedures as comments

--- run.py
+++ run.py
@@ -2,3 +2,17 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 print(f"Hello, Welcome to the Rock, Paper, Scissors Game!\nThis Game is between YOU and the COMPUTER......Who gonna winn??? Let's start and wish you both all the best!")
+
+#Game Procedures:
+
+#1. Tell the user to chose between R for Rock, or P for Paper or S for Scissors
+#2. Get the user's Input
+#3. Convert the users Input into capital letter (R, P and S)
+#4. Validate Inputs from the users and print an error if Invalid (Invalid are all choices different from R, P and S)
+#5. Let the computer make a choice( this will be a random choice from the R, P, S)
+#6. compare the choice of the user and of the computer
+#7. print the choices
+#8. Determine the winner for the played round by comparring the choices according the Games Rules
+#9. ask the user if they want to continue playing
+#10. if NOT, terminate the Game, other wise continue
+#11. After thrre rounds, declare the ultimate winner (who won twice in the 3 rounds)
