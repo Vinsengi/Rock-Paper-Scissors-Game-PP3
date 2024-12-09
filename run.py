2c198b695e82fe9381fb8eeaadabe0cb74591999
Vinsengi Ns <nvip2020@gmail.com>
2024-12-09 11:10:54 +0000

    Wrap On settings

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print(f"Hello, Welcome to the Rock, Paper, Scissors Game!\nThis Game is between YOU and the COMPUTER......Who gonna winn??? Let's start and wish you both all the best")
+print(f"Hello, Welcome to the Rock, Paper, Scissors Game!\nThis Game is between YOU and the COMPUTER......Who gonna winn??? Let's start and wish you both all the best!")
