cd861a5a9bf5874a830ca46ce3740b405865d09e
Vinsengi Ns <nvip2020@gmail.com>
2024-12-10 15:13:02 +0000

    Fix colors

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ background_color = bg("black") + fg("#0000ff")
 text_color = bg("white") + fg("black")
 text_style = attr("bold") 
 result_color = bg("red") 
-congs_color = bg("yellow")
+congs_color = bg("green") + fg("white")
 comp_congs_color = bg("red")
 cont_color = bg("White")
 print(f"{background_color }{text_style}ROCK-PAPER-SCISSORS-GAME-PP3")
@@ -162,7 +162,7 @@ while True:
     if user_score == 7: 
         print("------------------------------------------------")
         print("------------------------------------------------")
-        print(f"{congs_color}{text_style}GAME OVER!!!...\nCongratulations! You are the overall winner! with {user_score} won rounds! 🎉🎉🎉💃🕺💃🕺👯") 
+        print(f"{congs_color} GAME OVER!!!...\nCongratulations! You are the overall winner! with {user_score} won rounds! 🎉🎉🎉💃🕺💃🕺👯") 
         break 
     elif computer_score == 7: 
         print(f"{comp_congs_color}GAME OVER!! \nThe computer is the overall winner in the last {rounds_count} combined rounds with {computer_score} won rounds! Better luck next time! 😢") 
